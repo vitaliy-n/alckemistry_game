d12366c7b4418c125ec3b260b208f6ea25b04458
vnasa <vnasadiuk@gmail.com>
2026-01-22 19:43:15 +0200

    fix -- change app name

--- vite.config.ts
+++ vite.config.ts
@@ -5,5 +5,5 @@ import tailwindcss from '@tailwindcss/vite'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  base: '/alchemistry_game/',
+  base: '/alckemistry_game/',
 })
